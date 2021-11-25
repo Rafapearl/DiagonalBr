fd7954658ad08fd4d08d3a80dfacb684a12bdba6
Rafael <rafaironm2@gmail.com>
2021-11-25 04:41:50 -0300

    tentativa2

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const ytdl = require("ytdl-core")
+const fs = require('fs')
+llink = []
+
 
 app.set("view engine", "ejs");
 const path = require("path");
@@ -14,8 +18,20 @@ app.get("/", (req, res) => {
   res.render("../views/index");
 });
 
-
+app.get('/download', function (req, res) {
+  const {url} = req.body
+  res.header("Content-Disposition", 'attachmentt: filename="video.mp4"')
+  
+  return ytdl(url).pipe(fs.createWriteStream("video6.mp4"));
+})
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
 );
+
+app.post("/download", (req, res) => {
+  const {download, url  } = req.body;
+  llink.push({download, url})
+  message;
+  res.redirect("/");
+});
