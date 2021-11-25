9f503114aae16265447edc88e4d422f15f8c8590
Rafael <rafaironm2@gmail.com>
2021-11-25 18:15:40 -0300

    DownloadFuncionandoNoeDITOR

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+ const port = 3000;
 const ytdl = require("ytdl-core")
 const fs = require('fs')
-llink = []
+app.use(express.urlencoded())
+
 
 
 app.set("view engine", "ejs");
@@ -18,20 +19,27 @@ app.get("/", (req, res) => {
   res.render("../views/index");
 });
 
-app.get('/download', function (req, res) {
-  const {url} = req.body
-  res.header("Content-Disposition", 'attachmentt: filename="video.mp4"')
+// app.get('/download', function (req, res) {
+//   const {url} = req.body
+//   res.header("Content-Disposition", 'attachmentt: filename="video.mp4"')
   
-  return ytdl(url).pipe(fs.createWriteStream("video6.mp4"));
-})
+//   return ytdl(url).pipe(fs.createWriteStream("video6.mp4"));
+// })
+
+
+app.post("/", (req, res) => {
+  const { link  } = req.body
+  console.log(link)
+  if (!!link){
+    
+    return ytdl(link).pipe(fs.createWriteStream("video.mp4"));
+
+
+  }
+  //llink.push({download, url})
+  res.redirect("/");
+});
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
 );
-
-app.post("/download", (req, res) => {
-  const {download, url  } = req.body;
-  llink.push({download, url})
-  message;
-  res.redirect("/");
-});
