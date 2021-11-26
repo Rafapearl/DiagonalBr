24d97be4b0188005f6a826c57df8fa2c47032c6e
Juliane Pires <juliane.pires@lambda3.com.br>
2021-11-26 13:16:30 -0300

    Alterações no método post onde o vídeo é carregado e salvo no diretório público 'videos'

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
 const express = require("express");
-const app = express();
- const port = 3000;
-const ytdl = require("ytdl-core")
-const fs = require('fs')
-app.use(express.urlencoded())
-
-
-
-app.set("view engine", "ejs");
 const path = require("path");
+const ytdl = require("ytdl-core");
+const fs = require("fs");
+
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+app.use("/static", express.static("public"));
 
+app.set("view engine", "ejs");
 
-
+let url = "";
+let title = "";
 
 app.get("/", (req, res) => {
-  res.render("../views/index");
+  res.render("../views/index", { url });
 });
 
-// app.get('/download', function (req, res) {
-//   const {url} = req.body
-//   res.header("Content-Disposition", 'attachmentt: filename="video.mp4"')
-  
-//   return ytdl(url).pipe(fs.createWriteStream("video6.mp4"));
-// })
+app.post("/", async (req, res) => {
+  const { link } = req.body;
 
-
-app.post("/", (req, res) => {
-  const { link  } = req.body
-  console.log(link)
-  if (!!link){
-    
-    return ytdl(link).pipe(fs.createWriteStream("video.mp4"));
-
-
+  if (!!link) {
+    let info = await ytdl.getInfo(link);
+    url = info.videoDetails.embed.iframeUrl;
+    //SUBSTITUI OS CARACTERES ESPECIAIS
+    title = info.videoDetails.title.replaceAll(/\W/g, "");
+    return ytdl(link).pipe(fs.createWriteStream(`public/videos/${title}.mp4`));
   }
-  //llink.push({download, url})
+
+  res.redirect("/");
+});
+
+app.get("/video", (req, res) => {
+  res.render("../views/video", { src: `videos/${title}.mp4` });
+});
+
+app.post("/back", (req, res) => {
+  //DELETA O ARQUIVO AO RETORNAR À PÁGINA PRINCIPAL
+  fs.unlink(`public/videos/${title}.mp4`, (err) => {
+    if (err) throw err;
+    console.log("Arquivo deletado!");
+  });
+
   res.redirect("/");
 });
 
